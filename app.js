79a34bc5d9e0c43331497edf6d9fe480360ac241
Shota999 <ashottmarkarian@gmail.com>
2022-06-30 17:55:51 +0400

    main

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const cards = document.querySelectorAll(".memory_card");
+let flippedCard = false;
+let firstCard , secondCard;
+let boardLock = false;
+
+const flipCard = e => {
+    if(boardLock) return;
+
+    console.log(e.target.parentElement);
+    const target = e.target.parentElement;
+
+    target.classList.add("flip");
+
+    console.log(target.dataset.framework);
+
+    if (!flippedCard){
+
+        flippedCard = true;
+        firstCard = target;
+    } else{
+
+        flippedCard = false;
+        secondCard = target;
+
+        checkMatch();
+    }
+};
+const checkMatch = () =>{
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+        console.log("123");
+        firstCard.removeEventListener("click" , flipCard);
+        secondCard.removeEventListener("click" , flipCard);
+    }else{
+        boardLock = true;
+
+        setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+
+            boardLock = false;
+        }, 800);
+    }
+};
+
+cards.forEach(card => {
+    card.addEventListener("click" , flipCard)
+})
