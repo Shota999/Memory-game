1745d7b00a15f37ada21a65a76c59ff21e0a7b37
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-08-13 19:39:39 +0400

    main

--- app.js
+++ app.js
@@ -3,31 +3,27 @@ let firstCard, secondCard;
 let boardLock = false;
 
 const data = ['angular', 'aurelia', 'backbone', 'ember', 'react', 'vue', 'angular', 'aurelia', 'backbone', 'ember', 'react', 'vue'];
-
 const container = document.getElementById("memory_game");
 
-for (let i = 0; i < data.length; i++) {
-
-    container.innerHTML += `<div class="memory_card" data-framework="${data[i]}">
-    <img src="images/${data[i]}.svg" alt="${data[i]}" class="front_face">
-    <img src="images/js-badge.svg" alt="js-badge" class="back_face">
-    </div>`;
-}
-const cards = document.querySelectorAll(".memory_card");
-
-const image = document.querySelector(".memory_card");
 
 window.onload = () => randomImage(data);
-function randomImage(array){
-    let randomNum = Math.floor(Math.random() * array.length); 
-    image.setAttribute("data-framework", array[randomNum]);
-}
 
+const randomImage = (data) =>{
+    for (let i = 0; i < data.length; i++) {
+        let randomNum = Math.floor(Math.random() * data.length);
+        container.innerHTML += `<div class="memory_card" data-framework="${data[i]}">
+        <img src="images/${data[randomNum]}.svg" alt="${data[randomNum]}" class="front_face">
+        <img src="images/js-badge.svg" alt="js-badge" class="back_face">
+        </div>`;
+    }
+};
+
+const cards = document.querySelectorAll(".memory_card");
 
 const flipCard = e => {
     if (boardLock) return;
 
-    // console.log(e.target.parentElement);
+    console.log(e.target.parentElement);
     const target = e.target.parentElement;
 
     target.classList.add("flip");
