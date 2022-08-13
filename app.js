87c074acfb43759ece6df43995437b3e293b98c6
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-08-13 14:05:42 +0400

    main

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 
-const cards = document.querySelectorAll(".memory_card");
 let flippedCard = false;
-let firstCard , secondCard;
+let firstCard, secondCard;
 let boardLock = false;
 
+const data = ['angular', 'aurelia' , 'backbone' , 'ember' , 'react' , 'vue' ,'angular', 'aurelia' , 'backbone' , 'ember' , 'react' , 'vue'];
+
+const container = document.getElementById("memory_game");
+
+
+
+
+for ( let i = 0; i < data.length; i++){
+ 
+    container.innerHTML += `<div class="memory_card" data-framework="${data[i]}">
+    <img src="images/${data[i]}.svg" alt="${data[i]}" class="front_face">
+    <img src="images/js-badge.svg" alt="js-badge" class="back_face">
+    </div>`;
+}
+const cards = document.querySelectorAll(".memory_card");
+
 const flipCard = e => {
-    if(boardLock) return;
+    if (boardLock) return;
 
     console.log(e.target.parentElement);
     const target = e.target.parentElement;
@@ -14,11 +29,11 @@ const flipCard = e => {
 
     console.log(target.dataset.framework);
 
-    if (!flippedCard){
+    if (!flippedCard) {
 
         flippedCard = true;
         firstCard = target;
-    } else{
+    } else {
 
         flippedCard = false;
         secondCard = target;
@@ -26,12 +41,13 @@ const flipCard = e => {
         checkMatch();
     }
 };
-const checkMatch = () =>{
-    if(firstCard.dataset.framework === secondCard.dataset.framework){
+
+const checkMatch = () => {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
         console.log("123");
-        firstCard.removeEventListener("click" , flipCard);
-        secondCard.removeEventListener("click" , flipCard);
-    }else{
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    } else {
         boardLock = true;
 
         setTimeout(() => {
@@ -44,5 +60,5 @@ const checkMatch = () =>{
 };
 
 cards.forEach(card => {
-    card.addEventListener("click" , flipCard)
+    card.addEventListener("click", flipCard)
 })
